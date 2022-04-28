cdb7ecf4847011e37acf9f869efb11355abb75d8
shameem <shameem@shameems-MacBook-Pro.local>
2022-04-28 21:27:10 +0530

    changed server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const app = require('./App')
-const port = 'https://node-mongo-shameem.herokuapp.com/' || 3000;
+const port = 3000;
 
 const server = http.createServer(app)
 server.listen(port)
